5c158c92849860e5ae09e6df558332ee74276f89
mohsenrs <mohsen8022@gmail.com>
2021-03-05 10:03:02 +0330

    first commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      type="text"
+      required
+      placeholder="display name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {};
+
+    return { displayName, email, password, handleSubmit };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz6lPb9aRX4ca2y4l-Cj6olh6OEHXoyaY",
+  authDomain: "vue-site-15c8b.firebaseapp.com",
+  projectId: "vue-site-15c8b",
+  storageBucket: "vue-site-15c8b.appspot.com",
+  messagingSenderId: "208360284876",
+  appId: "1:208360284876:web:6ea2b8a984f177fca69869",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init firestore
+const projectFirestore = firebase.firestore();
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+
+export {projectFirestore, timestamp}
