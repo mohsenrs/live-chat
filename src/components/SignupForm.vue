4e47ad86c49b3e715fb76dbd20a32f54ab7e5298
mohsenrs <mohsen8022@gmail.com>
2021-03-06 14:51:52 +0330

    4th

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -8,21 +8,28 @@
     />
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
 
 <script>
 import { ref } from "vue";
+import useSignup from "../composables/useSignup";
+
 export default {
   setup() {
+    const { error, signup } = useSignup();
+
     const displayName = ref("");
     const email = ref("");
     const password = ref("");
 
-    const handleSubmit = () => {};
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+    };
 
-    return { displayName, email, password, handleSubmit };
+    return { displayName, email, password, handleSubmit, error };
   },
 };
 </script>
